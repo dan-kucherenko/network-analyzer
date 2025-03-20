8f6c7eabc51f92535812bcc60485e53c59f3b4dd
Daniil Kucherenko <daniil.ukraine@gmail.com>
2025-03-20 23:56:27 +0200

    feat: finish detecting the value of the properties

--- Sources/VisitorParser/Visitors/HttpVisitor.swift
+++ Sources/VisitorParser/Visitors/HttpVisitor.swift
@@ -1,22 +1,49 @@
 import SwiftSyntax
 
 class HttpVisitor: SyntaxVisitor {
-    struct PropertyImpact {
-        var found: Bool = false
-        var value: String?
-        var hasNetworkImpact: Bool = false
-        
-        var description: String {
-            return "Found: \(found), Value: \(value), Network Impact: \(hasNetworkImpact)"
-        }
-    }
-    
     private var properties: [String: PropertyImpact] = [
         "httpAdditionalHeaders": PropertyImpact(),
         "httpShouldSetCookies": PropertyImpact(),
         "httpCookieAcceptPolicy": PropertyImpact(),
         "httpShouldUsePipelining": PropertyImpact()
     ]
+    
+    override func visit(_ node: AssignmentExprSyntax) -> SyntaxVisitorContinueKind {
+        if let parentNode = node.parent?.as(ExprListSyntax.self) {
+            if let memberAccessNode = parentNode.first?.as(MemberAccessExprSyntax.self) {
+                let property = memberAccessNode.declName.baseName.text
+                
+                if properties.keys.contains(property),
+                   var propertyImpact = properties[property] {
+                    propertyImpact.found = true
+
+                    if let booleanLiteral = parentNode.last?.as(BooleanLiteralExprSyntax.self) {
+                        let boolValue = booleanLiteral.literal.text == "true"
+                        propertyImpact.value = booleanLiteral.literal.text
+                        
+                        switch property {
+                        case "httpShouldSetCookies":
+                            propertyImpact.hasNetworkImpact = boolValue
+                        case "httpShouldUsePipelining":
+                            propertyImpact.hasNetworkImpact = !boolValue
+                        default:
+                            break
+                        }
+                    } else if let enumCaseExpr = parentNode.last?.as(MemberAccessExprSyntax.self) {
+                        let enumCase = enumCaseExpr.declName.baseName.text
+                        
+                        if property == "httpCookieAcceptPolicy" {
+                            propertyImpact.value = enumCase
+                            propertyImpact.hasNetworkImpact = (enumCase.elementsEqual("never")) ? false : true
+                        }
+                    } else if property == "httpAdditionalHeaders" {
+                        propertyImpact.hasNetworkImpact = true
+                    }
+                }
+            }
+        }
+        return .visitChildren
+    }
 
     override func visit(_ node: MemberAccessExprSyntax) -> SyntaxVisitorContinueKind {
         if ((node.base?.as(DeclReferenceExprSyntax.self)) != nil) {
@@ -28,55 +55,20 @@ class HttpVisitor: SyntaxVisitor {
         return .visitChildren
     }
     
-    //    override func visit(_ node: InfixOperatorExprSyntax) -> SyntaxVisitorContinueKind {
-    //        if let assignExpr = element.as(InfixOperatorExprSyntax.self),
-    //           let memberAccess = assignExpr.leftOperand.as(MemberAccessExprSyntax.self) {
-    //            let property = memberAccess.declName.baseName.text
-    //
-    //            if properties.keys.contains(property),
-    //               let propertyImpact = properties[property] {
-    //                propertyImpact.found = true
-    //
-    //                if let booleanLiteral = assignExpr.rightOperand.as(BooleanLiteralExprSyntax.self) {
-    //                    let boolValue = booleanLiteral.literal.text == "true"
-    //                    propertyImpact.value = "Test Bool"
-    //                    print("Sequence Syntax visitor")
-    //
-    //                    switch property {
-    //                    case "httpShouldSetCookies":
-    //                        propertyImpact.hasNetworkImpact = boolValue
-    //                    case "httpShouldUsePipelining":
-    //                        propertyImpact.hasNetworkImpact = !boolValue
-    //                    default:
-    //                        break
-    //                    }
-    //                } else if let enumCaseExpr = assignExpr.rightOperand.as(MemberAccessExprSyntax.self) {
-    //                    let enumCase = enumCaseExpr.declName.baseName.text
-    //
-    //                    if property == "httpCookieAcceptPolicy" {
-    //                        propertyImpact.value = enumCase
-    //                        propertyImpact.hasNetworkImpact = true
-    //                    }
-    //                } else if property == "httpAdditionalHeaders" {
-    //                    propertyImpact.hasNetworkImpact = true
-    //                }
-    //            }
-    //        }
-    //        return .visitChildren
-    //    }
-    
-    func getPropertyStatus(for property: String) -> PropertyImpact? {
-        return properties[property]
-    }
-    
-    var hasNetworkImpactingConfigurations: Bool {
-        return properties.values.contains { $0.hasNetworkImpact }
-    }
-    
     func getImpactingSummary() -> [String] {
         return properties
             .compactMap { property, impactInfo -> String? in
                 "Property: \(property), impact info: \n\(impactInfo.description)"
             }
+    }
+}
+
+class PropertyImpact {
+    var found: Bool = false
+    var value: String?
+    var hasNetworkImpact: Bool = false
+    
+    var description: String {
+        return "Found: \(found), Value: \(value ?? "No information"), Network Impact: \(hasNetworkImpact)"
     }
 }
