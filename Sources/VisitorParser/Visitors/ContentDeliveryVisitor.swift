753e645f5e26d8cb3c0e07df05314b6e5331db09
Daniil Kucherenko <daniil.ukraine@gmail.com>
2025-03-26 00:05:38 +0200

    feat: implement the content delivery visitor

--- Sources/VisitorParser/Visitors/ContentDeliveryVisitor.swift
+++ Sources/VisitorParser/Visitors/ContentDeliveryVisitor.swift
@@ -1,21 +1,44 @@
 import SwiftSyntax
 
-class ContentDeliveryVisitor: SyntaxVisitor {
-    var foundHttpMaximumConnectionsPerHost = false
-    var foundAllowsExpensiveNetworkAccess = false
+class ContentDeliveryVisitor: SyntaxVisitor, Visitable {
+    var properties: [String: PropertyImpact] = [
+        "httpMaximumConnectionsPerHost": PropertyImpact(),
+        "allowsExpensiveNetworkAccess": PropertyImpact()
+    ]
+
+    override func visit(_ node: AssignmentExprSyntax) -> SyntaxVisitorContinueKind {
+        if let parentNode = node.parent?.as(ExprListSyntax.self) {
+            if let memberAccessNode = parentNode.first?.as(MemberAccessExprSyntax.self) {
+                let property = memberAccessNode.declName.baseName.text
+
+                if properties.keys.contains(property),
+                   var propertyImpact = properties[property] {
+                    propertyImpact.found = true
+
+                    if let intLiteral = parentNode.last?.as(IntegerLiteralExprSyntax.self),
+                       property == "httpMaximumConnectionsPerHost" {
+                        propertyImpact.value = intLiteral.literal.text
+                        propertyImpact.hasNetworkImpact = (Int(intLiteral.literal.text) ?? 0) > 6
+                    } else if let boolLiteral = parentNode.last?.as(BooleanLiteralExprSyntax.self),
+                              property == "allowsExpensiveNetworkAccess" {
+                        propertyImpact.value = boolLiteral.literal.text
+                        propertyImpact.hasNetworkImpact = boolLiteral.literal.text == "true"
+                    }
+
+                    properties[property] = propertyImpact
+                }
+            }
+        }
+        return .visitChildren
+    }
 
     override func visit(_ node: MemberAccessExprSyntax) -> SyntaxVisitorContinueKind {
-        let memberName = node.declName.baseName.text
-
-        switch memberName {
-        case "httpMaximumConnectionsPerHost":
-            foundHttpMaximumConnectionsPerHost = true
-        case "allowsExpensiveNetworkAccess":
-            foundAllowsExpensiveNetworkAccess = true
-        default:
-            break
+        if ((node.base?.as(DeclReferenceExprSyntax.self)) != nil) {
+            let property = node.declName.baseName.text
+            if properties.keys.contains(property) {
+                properties[property]?.found = true
+            }
         }
-
         return .visitChildren
     }
 }
