174ff7961dc3e1bad4e84a40622434cfd8e88805
Daniil Kucherenko <daniil.ukraine@gmail.com>
2025-03-25 23:32:59 +0200

    feat: add a visitor manager for multiple visitors

---
+++ Sources/VisitorParser/VisitorManager.swift
@@ -0,0 +1,23 @@
+import SwiftSyntax
+
+class VisitorManager {
+    private let filePath: String
+    private let outputPath: String
+    let visitors: [Visitable]
+    
+    init(filePath: String, outputPath: String) {
+        self.filePath = filePath
+        self.outputPath = outputPath
+        self.visitors = [
+            HttpVisitor(viewMode: .all),
+            ConnectionVisitor(viewMode: .all)
+        ]
+    }
+    
+    func analyzeSyntaxTree(_ tree: SourceFileSyntax) -> [String] {
+        visitors.map { visitor in
+            visitor.walk(tree)
+            return visitor.getImpactingSummary()
+        }.flatMap { $0 }
+    }
+}
