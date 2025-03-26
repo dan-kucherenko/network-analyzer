6c6afaa323eb67ff889ae865703b0c435590fa3d
Daniil Kucherenko <daniil.ukraine@gmail.com>
2025-03-26 22:40:22 +0200

    feat: implement data caching visitor

--- Sources/VisitorParser/Visitors/DataCachingVisitor.swift
+++ Sources/VisitorParser/Visitors/DataCachingVisitor.swift
@@ -1,18 +1,39 @@
 import SwiftSyntax
 
-class DataCachingVisitor: SyntaxVisitor {
-    var foundUrlCache = false
-    var foundRequestCachePolicy = false
+class DataCachingVisitor: SyntaxVisitor, Visitable {
+    var properties: [String: PropertyImpact] = [
+        "urlCache": PropertyImpact(),
+        "requestCachePolicy": PropertyImpact()
+    ]
+    
+    override func visit(_ node: AssignmentExprSyntax) -> SyntaxVisitorContinueKind {
+        if let parentNode = node.parent?.as(ExprListSyntax.self) {
+            if let memberAccessNode = parentNode.first?.as(MemberAccessExprSyntax.self) {
+                let property = memberAccessNode.declName.baseName.text
+                
+                if properties.keys.contains(property),
+                   let propertyImpact = properties[property] {
+                    propertyImpact.found = true
+
+                    if let booleanLiteral = parentNode.last?.as(BooleanLiteralExprSyntax.self) {
+                        propertyImpact.value = booleanLiteral.literal.text
+                        
+                        propertyImpact.hasNetworkImpact = true
+                    } else if property == "urlCache" || property == "requestCachePolicy" {
+                        propertyImpact.hasNetworkImpact = true
+                    }
+                }
+            }
+        }
+        return .visitChildren
+    }
 
     override func visit(_ node: MemberAccessExprSyntax) -> SyntaxVisitorContinueKind {
-        let memberName = node.declName.baseName.text
-
-        if memberName == "urlCache" {
-            foundUrlCache = true
-        } else if memberName == "requestCachePolicy" {
-            foundRequestCachePolicy = true
+        let property = node.declName.baseName.text
+        if properties.keys.contains(property) {
+            properties[property]?.found = true
+            properties[property]?.hasNetworkImpact = true
         }
-
         return .visitChildren
     }
 }
