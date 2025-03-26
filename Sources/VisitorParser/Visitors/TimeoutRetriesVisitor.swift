6cc9f6935c4a4d9d940a65913a09048cd409b226
Daniil Kucherenko <daniil.ukraine@gmail.com>
2025-03-26 22:41:00 +0200

    feat: implement timeout visitor

--- Sources/VisitorParser/Visitors/TimeoutRetriesVisitor.swift
+++ Sources/VisitorParser/Visitors/TimeoutRetriesVisitor.swift
@@ -1,18 +1,50 @@
 import SwiftSyntax
 
-class TimeoutRetriesVisitor: SyntaxVisitor {
-    var foundTimeoutRequest = false
-    var foundTimeoutResource = false
+class TimeoutAndRetryVisitor: SyntaxVisitor, Visitable {
+    var properties: [String: PropertyImpact] = [
+        "timeoutIntervalForRequest": PropertyImpact(),
+        "timeoutIntervalForResource": PropertyImpact()
+    ]
     
-    override func visit(_ node: MemberAccessExprSyntax) -> SyntaxVisitorContinueKind {
-        let memberName = node.declName.baseName.text
-        
-        if memberName == "timeoutIntervalForRequest" {
-            foundTimeoutRequest = true
-        } else if memberName == "timeoutIntervalForResource" {
-            foundTimeoutResource = true
+    override func visit(_ node: AssignmentExprSyntax) -> SyntaxVisitorContinueKind {
+        if let parentNode = node.parent?.as(ExprListSyntax.self) {
+            if let memberAccessNode = parentNode.first?.as(MemberAccessExprSyntax.self) {
+                let property = memberAccessNode.declName.baseName.text
+                
+                if properties.keys.contains(property),
+                   let propertyImpact = properties[property] {
+                    propertyImpact.found = true
+                    
+                    if let numberLiteral = parentNode.last?.as(IntegerLiteralExprSyntax.self) {
+                        let timeoutValue = Int(numberLiteral.literal.text)
+                        propertyImpact.value = numberLiteral.literal.text
+                        
+                        switch property {
+                        case "timeoutIntervalForRequest":
+                            if let timeout = timeoutValue, timeout < 30 {
+                                propertyImpact.hasNetworkImpact = true
+                            }
+                        case "timeoutIntervalForResource":
+                            if let timeout = timeoutValue, timeout < 30 {
+                                propertyImpact.hasNetworkImpact = true
+                            }
+                        default:
+                            break
+                        }
+                    }
+                }
+            }
         }
-        
+        return .visitChildren
+    }
+
+    override func visit(_ node: MemberAccessExprSyntax) -> SyntaxVisitorContinueKind {
+        if ((node.base?.as(DeclReferenceExprSyntax.self)) != nil) {
+            let property = node.declName.baseName.text
+            if properties.keys.contains(property) {
+                properties[property]?.found = true
+            }
+        }
         return .visitChildren
     }
 }
