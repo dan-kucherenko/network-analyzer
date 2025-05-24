238c2020e857c7b35513f5bf4f48dc116642a568
Daniil Kucherenko <daniil.ukraine@gmail.com>
2025-05-24 18:47:00 +0300

    ref: timeout visitor

--- Sources/VisitorParser/URLVisitors/TimeoutRetriesVisitor.swift
+++ Sources/VisitorParser/URLVisitors/TimeoutRetriesVisitor.swift
@@ -1,10 +1,11 @@
 import SwiftSyntax
 
 class TimeoutAndRetryVisitor: SyntaxVisitor, Visitable {
-    var properties: [String: PropertyImpact] = [
-        "timeoutIntervalForRequest": PropertyImpact(),
-        "timeoutIntervalForResource": PropertyImpact()
+    var properties: [String] = [
+        "timeoutIntervalForRequest",
+        "timeoutIntervalForResource"
     ]
+    var warnings: [XcodeDiagnostic] = []
     
     private let filePath: String
     
@@ -19,45 +20,49 @@ class TimeoutAndRetryVisitor: SyntaxVisitor, Visitable {
                 let property = memberAccessNode.declName.baseName.text
                 let location = node.startLocation(converter: SourceLocationConverter(fileName: filePath, tree: node.root))
                 
-                if properties.keys.contains(property),
-                   let propertyImpact = properties[property] {
-                    propertyImpact.found = true
+                if properties.contains(property),
+                   let numberLiteral = parentNode.last?.as(IntegerLiteralExprSyntax.self) {
+                    let timeoutValue = Int(numberLiteral.literal.text)
                     
-                    if let numberLiteral = parentNode.last?.as(IntegerLiteralExprSyntax.self) {
-                        let timeoutValue = Int(numberLiteral.literal.text)
-                        propertyImpact.value = numberLiteral.literal.text
-                        
-                        switch property {
-                        case "timeoutIntervalForRequest":
-                            if let timeout = timeoutValue, timeout < 30 || timeout > 120 {
-                                propertyImpact.hasNetworkImpact = true
-                                propertyImpact.location.append((line: location.line, column: location.column))
-                                propertyImpact.recommendation = "Consider setting a more appropriate timeout value between 30 and 120 seconds. Property controls how long (in seconds) a task should wait for additional data. Default value is 60 seconds"
-                            }
-                        case "timeoutIntervalForResource":
-                            if let timeout = timeoutValue, timeout < 3600 || timeout > 3600 * 60 * 3 {
-                                propertyImpact.hasNetworkImpact = true
-                                propertyImpact.location.append((line: location.line, column: location.column))
-                                propertyImpact.recommendation = "Consider setting a more appropriate timeout value between 1 hour and 8 days. Property controls how long (in seconds) to wait for a complete resource to transfer before giving up. Default value is 7 days"
-                            }
-                        default:
-                            break
+                    switch property {
+                    case "timeoutIntervalForRequest":
+                        if let timeout = timeoutValue, timeout < 30 || timeout > 120 {
+                            warnings.append(XcodeDiagnostic(
+                                filePath: filePath,
+                                line: location.line,
+                                column: location.column,
+                                message: "Consider setting a more appropriate timeout value between 30 and 120 seconds. Property controls how long (in seconds) a task should wait for additional data. Default value is 60 seconds. Current value is: \(timeout) seconds"
+                            ))
                         }
+                    case "timeoutIntervalForResource":
+                        if let timeout = timeoutValue, timeout < 3600 || timeout > 3600 * 60 * 3 {
+                            warnings.append(XcodeDiagnostic(
+                                filePath: filePath,
+                                line: location.line,
+                                column: location.column,
+                                message: "Consider setting a more appropriate timeout value between 1 hour and 8 days. Property controls how long (in seconds) to wait for a complete resource to transfer before giving up. Default value is 7 days. Current value is: \(timeout) seconds"
+                            ))
+                        }
+                    default:
+                        break
                     }
                 }
             }
         }
         return .visitChildren
     }
-
+    
     override func visit(_ node: MemberAccessExprSyntax) -> SyntaxVisitorContinueKind {
         if ((node.base?.as(DeclReferenceExprSyntax.self)) != nil) {
             let property = node.declName.baseName.text
-            if properties.keys.contains(property) {
+            if properties.contains(property) {
                 let location = node.startLocation(converter: SourceLocationConverter(fileName: filePath, tree: node.root))
-                let propertyImpact = properties[property]
-                propertyImpact?.found = true
-                propertyImpact?.location.append((line: location.line, column: location.column))
+                warnings.append(XcodeDiagnostic(
+                    filePath: filePath,
+                    line: location.line,
+                    column: location.column,
+                    message: "Timeout property '\(property)' is being accessed. Make sure to set appropriate timeout values."
+                ))
             }
         }
         return .visitChildren
