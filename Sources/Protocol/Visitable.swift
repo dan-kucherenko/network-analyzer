b061da5d19d14d321f82ce0c841cf31fb3e8f7eb
Daniil Kucherenko <daniil.ukraine@gmail.com>
2025-05-25 14:12:12 +0300

    ref: remove the unused method

--- Sources/Protocol/Visitable.swift
+++ Sources/Protocol/Visitable.swift
@@ -3,13 +3,4 @@ import SwiftSyntax
 protocol Visitable: SyntaxVisitor {
     var properties: [String] { get set }
     var warnings: [AntipatternWarning] { get set }
-//    func getImpactingSummary(properties: [String: PropertyImpact]) -> [String]
 }
-
-//extension Visitable {
-//    func getImpactingSummary(properties: [String: PropertyImpact]) -> [String] {
-//        properties.compactMap { property, impactInfo -> String? in
-//            "Property: \(property), impact info: \n\(impactInfo.description)"
-//        }
-//    }
-//}
